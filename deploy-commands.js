17136d13f0f8c92a4d61867027e57b998839ddd0
tobyxxzz <pedrolulapedra@gmail.com>
2025-12-26 21:35:46 -0300

    Update deploy-commands.js

--- deploy-commands.js
+++ deploy-commands.js
@@ -1,30 +1,33 @@
-const { REST, Routes } = require('discord.js');
-require('dotenv').config();
+const { REST, Routes } = require('discord.js')
+require('dotenv').config()
 
 const commands = [
-    {
-        name: 'start',
-        description: 'Inicia o envio de links aleatórios a cada segundo.',
-    },
-    {
-        name: 'stop',
-        description: 'Para o envio de links.',
-    },
-];
+  {
+    name: 'start',
+    description: 'Inicia o envio de links aleatórios.',
+  },
+  {
+    name: 'stop',
+    description: 'Para o envio de links.',
+  },
+]
 
-const rest = new REST({ version: '10' }).setToken(process.env.DISCORD_TOKEN);
+const rest = new REST({ version: '10' }).setToken(process.env.TOKEN)
 
-(async () => {
-    try {
-        console.log('Started refreshing application (/) commands.');
+;(async () => {
+  try {
+    console.log('Started refreshing application (/) commands.')
 
-        await rest.put(
-            Routes.applicationGuildCommands(process.env.CLIENT_ID, process.env.GUILD_ID),
-            { body: commands },
-        );
+    await rest.put(
+      Routes.applicationGuildCommands(
+        process.env.CLIENT_ID,
+        process.env.GUILD_ID
+      ),
+      { body: commands }
+    )
 
-        console.log('Successfully reloaded application (/) commands.');
-    } catch (error) {
-        console.error(error);
-    }
-})();
+    console.log('Successfully reloaded application (/) commands.')
+  } catch (error) {
+    console.error(error)
+  }
+})()
